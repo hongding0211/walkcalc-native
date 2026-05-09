d8957f6569baab92a33ccf783b7c72ae900f5944
hongding0211 <keith.dh@hotmail.com>
2026-05-09 23:48:31 +0800

    Initial Commit

---
+++ walkcalc-native/walkcalc_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  walkcalc_nativeApp.swift
+//  walkcalc-native
+//
+//  Created by hong on 2026/5/9.
+//
+
+import SwiftUI
+
+@main
+struct walkcalc_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
